7bf0fb37598d01076f187c507bbe9be60e800fd4
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-03 21:25:27 +0530

    program4

---
+++ program4.py
@@ -0,0 +1,21 @@
+employeenamelist=[]
+employeeagelist=[]
+employeedesignationlist=[]
+employeesalarylist=[]
+for i in range(0,1):
+    name=input("enter name:")
+    employeenamelist.append(name)
+print(employeenamelist)
+for i in range(0,1):
+    age=input("enter age:")
+    employeeagelist.append(age)
+print(employeeagelist)
+for i in range(0,1):
+    designation=input("enter designation:")
+    employeedesignationlist.append(designation)
+print(employeedesignationlist)
+for i in range(0,1):
+    salary=input("enter salary:")
+    employeesalarylist.append(salary)
+print(employeesalarylist)
+
