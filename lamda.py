56f5c8fd58e776f17e504cf278eeec7bc8d864f4
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-07 12:22:43 +0530

    lamda3num

--- lamda.py
+++ lamda.py
@@ -2,3 +2,9 @@ x=12
 multiply= lambda a:a*5
 y=multiply(x)
 print(y)
+
+z=12
+v=13
+add=lambda a,b:a+b
+y=add(z,v)
+print(y)
---
+++ lamda3num.py
@@ -0,0 +1,6 @@
+x=12
+y=12
+z=99
+sum=lambda a,b,c:a+b+c
+v=sum(x,y,z)
+print(v)
